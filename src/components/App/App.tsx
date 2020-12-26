878d2a74b94865caf40b24022be7e7d3c8f8d52c
erick Monteiro <monteiro.ck@hotmail.com>
2020-12-26 15:52:02 -0300

    primeiro commit

---
+++ src/components/App/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Header from '../Header';
+//import TestComponent from './components/TestComponent/TestComponent'
+
+function App() {
+  return (
+    <div className="App">
+      <Header title="AlgaStock" />
+      dsadsada
+    </div>
+  );
+}
+
+export default App;
