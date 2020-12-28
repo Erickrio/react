33ccdcfb4a3bd361552015cb56d8a7aa1498b439
erick Monteiro <monteiro.ck@hotmail.com>
2020-12-28 13:13:49 -0300

    interação de listas de dados - usando map

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -18,21 +18,18 @@ function App() {
       <Header title="AlgaStock" />
 
       <Container>
-        <Button
-          onClick={() => window.alert('UIIIU')}
-          appendIcon={<TestComponent />}
-        >
-          Alert
-        </Button>
-        <Input
-          label="Street"
-          placeholder="E.g.: 15h Avenue"
-          value={street}
-          onChange={e => setStreet(e.target.value)}
-        />
+        <ul>
+          {
+            ['Erick','Thais','Bob','Bob'].map((name,index) =>{
+              return <li key={index}>
+                {name}
+              </li>
+            })
+          }
+        </ul>
       </Container>
     </div>
   );
 }
-
+//iteração de arrays utilizando map - interação de lista de dados
 export default App;
