3a0be76daf5716f22ebaf1f1423ec48060c4f6e7
erick Monteiro <monteiro.ck@hotmail.com>
2021-03-26 14:50:46 -0300

    produtividade c axius

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
+import http from './utils/http'
+
+//API DO FETCH
+//fetch('https://jsonplaceholder.typicode.com/posts')
+
+// http.get('/posts')
+// http.get('/posts/1')
+// http.get('https://viacep.com.br/ws/200000/json/')
 
 ReactDOM.render(
   <React.StrictMode>
