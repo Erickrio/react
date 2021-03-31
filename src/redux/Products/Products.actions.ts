a9b34fa049100d3cbf2a7df72e7a2221c8feb3f6
erick Monteiro <monteiro.ck@hotmail.com>
2021-03-31 10:13:57 -0300

    disparando actions com componentes

--- src/redux/Products/Products.actions.ts
+++ src/redux/Products/Products.actions.ts
@@ -1,14 +1,9 @@
+import { ProductCreator } from './../../components/Products/ProductForm';
 import { Action } from "./Products.reducer"
-import { Product } from "../../shared/Table/Table.mockdata"
 
-export const insertNewProduct = (): Action<Product> => {
+export const insertNewProduct = (payload: ProductCreator): Action<ProductCreator> => {
   return {
     type: 'INSERT_NEW_PRODUCT',
-    payload: {
-      _id: '123ldjksa',
-      name: 'Cookie',
-      price: 0.35,
-      stock: 700
-    }
+    payload
   }
 }
