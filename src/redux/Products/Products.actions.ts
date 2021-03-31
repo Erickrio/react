a9058fcbe86c4851b2701378e6c04801db4d9493
erick Monteiro <monteiro.ck@hotmail.com>
2021-03-31 11:29:55 -0300

    actions assincronas usando redux thunk

--- src/redux/Products/Products.actions.ts
+++ src/redux/Products/Products.actions.ts
@@ -1,6 +1,15 @@
 import { ProductCreator } from './../../components/Products/ProductForm';
 import { Action } from "./Products.reducer"
+import { getAllProducts } from "../../services/Products.service"
 
+
+export const getProducts = () => async (dispatch: any) =>{
+  const products = await getAllProducts()
+  dispatch({
+    type: 'FETCH_PRODUCTS',
+    payload: products
+  })
+}
 export const insertNewProduct = (payload: ProductCreator): Action<ProductCreator> => {
   return {
     type: 'INSERT_NEW_PRODUCT',
--- src/redux/index.ts
+++ src/redux/index.ts
@@ -1,15 +1,22 @@
-  
-import { createStore, combineReducers  } from 'redux'
+import { 
+  createStore,
+  combineReducers,
+  compose,
+  applyMiddleware  } from 'redux'
 import Products from './Products/Products.reducer'
-
+import thunk from 'redux-thunk'
 const reducers = combineReducers({
   products: Products
 })
 
 const store = createStore(
   reducers,
-  // @ts-ignore
+  compose(
+    applyMiddleware(thunk),
+     // @ts-ignore
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+ 
 )
 
 export default store
